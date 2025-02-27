d853d365473b443cd0c92d6202a67fa01dbf2b2e
sudo-dqr <3577669408@qq.com>
2025-02-27 12:44:42 +0800

    [Feat]: P2241 AC

--- luogu_brute/P2241_calculating_square.cpp
+++ luogu_brute/P2241_calculating_square.cpp
@@ -3,8 +3,13 @@
 int main() {
   int n, m;
   std::cin >> n >> m;
-  int cnt1 = 0, cnt2 = 0;
-  
-  std::cout << cnt1 << ' ' << cnt2 << std::endl;
+  long long cnt1 = 0, cnt2 = 0;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      cnt1 += i * j;
+      cnt2 += (i > j) ? j : i;
+    }
+  }
+  std::cout << cnt2 << ' ' << cnt1 - cnt2 << std::endl;
   return 0;
 }
